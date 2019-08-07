bcfd8edbe475aa0cfcff6021f9e1cb3164ba2cb8
Default user <id.vargas@uniandes.edu.co>
2019-08-07 16:47:22 +0000

    primer punto

--- S1C1.py
+++ S1C1.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 lista = [5,3,2,8,1,4]
-
+print (lista)
 for i in range (len(lista)):
-    if lista[i]%2 != 0:
-        if lista[i] > lista[i+1]:
-            
-        
+    for j in range(i+1, len(lista)):
+        if lista[i] > lista[j]: 
+            if lista[i]%2 != 0 and lista[j]%2 !=0:
+                var = lista[i]
+                lista[i] = lista[j]
+                lista[j] = var
+print(lista)
+
+                
+          
