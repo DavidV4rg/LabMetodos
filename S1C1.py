4fa41b7ba4c32083d03e2df7e1af858d5c6da50f
Default user <id.vargas@uniandes.edu.co>
2019-08-07 16:57:34 +0000

    segundo punto

--- S1C1.py
+++ S1C1.py
@@ -2,6 +2,8 @@ import numpy as np
 
 lista = [5,3,2,8,1,4]
 print (lista)
+
+#Primer punto
 for i in range (len(lista)):
     for j in range(i+1, len(lista)):
         if lista[i] > lista[j]: 
@@ -11,5 +13,23 @@ for i in range (len(lista)):
                 lista[j] = var
 print(lista)
 
-                
-          
+#Segundo punto
+print ("")
+l= [5,3,2,8,1,4]
+print (l)
+
+for i in range (len(l)):
+    for j in range(i+1, len(l)):
+        if l[i] > l[j]: 
+            if l[i]%2 != 0 and l[j]%2 !=0:
+                variable = l[i]
+                l[i] = l[j]
+                l[j] = variable
+        else:
+            if l[i]%2==0 and l[j]%2 ==0:
+                variable = l[j]
+                l[j] = l[i]
+                l[i] = variable                            
+                             
+print(l)
+        
