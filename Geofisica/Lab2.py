206541a169cfa885f0a2fe0b3fad00a47b253085
Default user <id.vargas@uniandes.edu.co>
2019-09-06 17:39:07 +0000

    aja

--- Geofisica/Lab2.py
+++ Geofisica/Lab2.py
@@ -3,4 +3,12 @@ import matplotlib.pylab as plt
 
 a = np.genfromtxt("dato1.txt", delimiter = "|")
 
-print(a)
+x = a[:,0]
+m = a[:,1]
+
+plt.figure()
+plt.plot(x,m)
+plt.grid()
+plt.xlabel("$x(m)$")
+plt.ylabel("$Elevación$ $(m)$")
+plt.savefig("elevacion")
